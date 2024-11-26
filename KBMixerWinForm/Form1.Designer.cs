351d75b4d7fd63eed1d0adf7bacc6f1864cd0a0f
Chris Sanford <ChrisSanfordBusiness@gmail.com>
2024-11-25 21:15:29 -0500

    Populate App Audio Output Sessions

--- KBMixerWinForm/Form1.Designer.cs
+++ KBMixerWinForm/Form1.Designer.cs
@@ -29,7 +29,9 @@
         private void InitializeComponent()
         {
             deviceComboBox = new ComboBox();
-            label1 = new Label();
+            deviceLabel = new Label();
+            appComboBox = new ComboBox();
+            appLabel = new Label();
             SuspendLayout();
             // 
             // deviceComboBox
@@ -41,26 +43,50 @@
             deviceComboBox.FormattingEnabled = true;
             deviceComboBox.Location = new Point(60, 6);
             deviceComboBox.Name = "deviceComboBox";
-            deviceComboBox.Size = new Size(121, 23);
+            deviceComboBox.Size = new Size(423, 23);
             deviceComboBox.TabIndex = 0;
-            deviceComboBox.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            deviceComboBox.SelectedIndexChanged += deviceComboBox_SelectedIndexChanged;
             // 
-            // label1
+            // deviceLabel
             // 
-            label1.AutoSize = true;
-            label1.Location = new Point(12, 9);
-            label1.Name = "label1";
-            label1.Size = new Size(42, 15);
-            label1.TabIndex = 1;
-            label1.Text = "Device";
-            label1.Click += label1_Click;
+            deviceLabel.AutoSize = true;
+            deviceLabel.Location = new Point(12, 9);
+            deviceLabel.Name = "deviceLabel";
+            deviceLabel.Size = new Size(42, 15);
+            deviceLabel.TabIndex = 1;
+            deviceLabel.Text = "Device";
+            deviceLabel.Click += label1_Click;
+            // 
+            // appComboBox
+            // 
+            appComboBox.AccessibleDescription = "App";
+            appComboBox.AccessibleName = "App";
+            appComboBox.AccessibleRole = AccessibleRole.ComboBox;
+            appComboBox.DropDownStyle = ComboBoxStyle.DropDownList;
+            appComboBox.FormattingEnabled = true;
+            appComboBox.Location = new Point(60, 35);
+            appComboBox.Name = "appComboBox";
+            appComboBox.Size = new Size(423, 23);
+            appComboBox.TabIndex = 2;
+            // 
+            // appLabel
+            // 
+            appLabel.AutoSize = true;
+            appLabel.Location = new Point(12, 38);
+            appLabel.Name = "appLabel";
+            appLabel.Size = new Size(29, 15);
+            appLabel.TabIndex = 3;
+            appLabel.Text = "App";
+            appLabel.Click += label2_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(label1);
+            Controls.Add(appLabel);
+            Controls.Add(appComboBox);
+            Controls.Add(deviceLabel);
             Controls.Add(deviceComboBox);
             Name = "Form1";
             Text = "KBMixer";
@@ -71,6 +97,8 @@
         #endregion
 
         private ComboBox deviceComboBox;
-        private Label label1;
+        private Label deviceLabel;
+        private ComboBox appComboBox;
+        private Label appLabel;
     }
 }
--- KBMixerWinForm/Form1.cs
+++ KBMixerWinForm/Form1.cs
@@ -2,8 +2,12 @@ using NAudio.CoreAudioApi;
 
 namespace KBMixerWinForm
 {
+    // the Form1 class is defined as a partial class to separate the auto-generated code
+    // (created by the Windows Forms Designer) from the custom code defined here
     public partial class Form1 : Form
     {
+        private MMDevice selectedDevice; // Store the selected device object in a class-level scope
+
         public Form1()
         {
             InitializeComponent();
@@ -23,15 +27,41 @@ namespace KBMixerWinForm
             if (deviceComboBox.Items.Count > 0)
             {
                 deviceComboBox.SelectedIndex = 0;
+                selectedDevice = devices[0]; // Set the selected device object
+                PopulateAudioOutputSessions();
             }
         }
 
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        private void PopulateAudioOutputSessions()
+        {
+            appComboBox.Items.Clear();
+
+            if (selectedDevice != null) // Access the selected device object
+            {
+                AudioSessionManager sessionManager = selectedDevice.AudioSessionManager;
+                var sessions = sessionManager.Sessions;
+
+                for (int i = 0; i < sessions.Count; i++)
+                {
+                    appComboBox.Items.Add(sessions[i].DisplayName);
+                }
+            }
+        }
+
+        private void deviceComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            MMDeviceEnumerator enumerator = new MMDeviceEnumerator();
+            MMDeviceCollection devices = enumerator.EnumerateAudioEndPoints(DataFlow.Render, DeviceState.Active);
+            selectedDevice = devices[deviceComboBox.SelectedIndex]; // Update the selected device object
+            PopulateAudioOutputSessions();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void label2_Click(object sender, EventArgs e)
         {
 
         }
